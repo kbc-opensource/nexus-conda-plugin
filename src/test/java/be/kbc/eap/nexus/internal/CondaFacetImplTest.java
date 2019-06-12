e0a62fde358bbe1fda7662222196333d2d34a18a
Wim Van Leuven <wim.vanleuven@kbc.be>
2019-06-12 14:21:53 +0200

    use object serializer gson for json strings

--- src/test/java/be/kbc/eap/nexus/internal/CondaFacetImplTest.java
+++ src/test/java/be/kbc/eap/nexus/internal/CondaFacetImplTest.java
@@ -2,6 +2,7 @@ package be.kbc.eap.nexus.internal;
 
 import be.kbc.eap.nexus.CondaPath;
 import be.kbc.eap.nexus.CondaPathParser;
+import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 import org.sonatype.nexus.repository.view.Payload;
@@ -15,12 +16,14 @@ public class CondaFacetImplTest {
 
     private CondaFacetImpl facet;
     private CondaPathParser parser;
+    private Gson gson;
 
     @Before
     public void setUp() throws Exception {
         final Map<String, CondaPathParser> condaPathParserMap = Collections.emptyMap();
         facet = new CondaFacetImpl(condaPathParserMap);
         parser = new CondaPathParserImpl();
+        gson = new Gson();
     }
 
 
@@ -28,12 +31,7 @@ public class CondaFacetImplTest {
     public void testPut() throws IOException {
         CondaPath path = this.parser.parsePath("noarch/test_prj-0.0.1.dev-py27_20190611100529.tar.bz2");
         Payload content = new StringPayload("some content", "application/x-tar");
-        String indexJson = "{\"size\": 3772, \"subdir\": \"noarch\", \"build_number\": 20190611100529, "
-                + "\"name\": \"test_prj\", \"license\": \"WTFPL\", \"timestamp\": 1560247671929, "
-                + "\"noarch\": \"generic\", \"depends\": [\"python >=2.7,<2.8.0a0\"], \"version\": \"0.0.1.dev\", "
-                + "\"build\": \"py27_20190611100529\", \"sha256\": "
-                + "\"1f36c3ef83988b29e5c8dbeb9751cf3a55fbd4353c803c6b3b8dae79a019669e\", "
-                + "\"md5\": \"5ce9195417e11bed705b23f4a7008b62\"}";
+        String indexJson = gson.toJson(new IndexJson());
 
         facet.put(path, content, indexJson);
     }
@@ -48,7 +46,7 @@ class IndexJson {
     String license = "WTFPL";
     long timestamp = 1560247671929L;
     String noarch = "generic";
-    String[] depends = ["python >=2.7,<2.8.0a0"];
+    String[] depends = { "python >=2.7,<2.8.0a0" };
     String version = "0.0.1.dev";
     String build = "py27_20190611100529";
     String sha256 = "1f36c3ef83988b29e5c8dbeb9751cf3a55fbd4353c803c6b3b8dae79a019669e";
